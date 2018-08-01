dc8f5766d1a59a10c1c88b9ea210907d08619c67
WangNingkai <1655586865@qq.com>
2018-08-01 12:55:21 +0800

    fix

--- src/MarkdownEditorWidget.php
+++ src/MarkdownEditorWidget.php
@@ -4,6 +4,9 @@ namespace WangNingkai\MarkdownEditor;
 use yii\bootstrap\InputWidget;
 use yii\helpers\Html;
 use yii\helpers\Json;
+use yii\helpers\Inflector;
+use yii\web\JsExpression;
+
 
 class MarkdownEditorWidget extends InputWidget
 {
@@ -14,64 +17,79 @@ class MarkdownEditorWidget extends InputWidget
     public $clientOptions = [];
 
     /**
+     * {@inheritDoc}
+     * @see \yii\base\Object::init()
+     */
+    public function init()
+    {
+        parent::init();
+        if (!isset ($this->options ['id'])) {
+            $this->options ['id'] = $this->getId();
+        }
+       $this->initClientOptions();
+    }
+
+
+    /**
      * Renders the widget.
      */
     public function run()
     {
         if ($this->hasModel()) {
-            $this->name = empty($this->options['name']) ? Html::getInputName($this->model, $this->attribute) :
-                $this->options['name'];
-            $this->value = Html::getAttributeValue($this->model, $this->attribute);
+            $textarea = Html::activeTextArea($this->model, $this->attribute, $this->options);
         }
-        echo Html::tag('div', '', $this->options);
+        $textarea = Html::textArea($this->name, $this->value, $this->options);
         $this->registerClientScript();
+    }
+
+
+    public function initClientOptions()
+    {
+        $options = [
+            'watch' => true,
+            'emoji' => true,
+            'syncScrolling' => true,
+            'searchReplace' => true,
+            'taskList' => true,
+            'tocm' => true,
+            'tex' => true,
+            'flowChart' => true,
+            'sequenceDiagram' => true,
+            'height' => "600",
+            'htmlDecode' => "style,script,iframe|on*",
+            'placeholder' => "欢迎使用MarkDown编辑器",
+            'toolbarIcons' => [
+                "undo", "redo", "|",
+                "h1", "h2", "h3", "h4", "h5", "h6", "|",
+                "bold", "del", "italic", "quote", "list-ul", "list-ol", "hr", "pagebreak", "|",
+                "code", "preformatted-text", "code-block", "|",
+                "image", "table", "link", "reference-link", "|",
+                "datetime", "emoji", "html-entities", "|",
+                "search", "goto-line", "ucwords", "uppercase", "lowercase", "clear", "|",
+                "preview", "watch", "fullscreen", "|",
+                "help"
+            ],
+        ];
+        $this->clientOptions = array_merge($options, $this->clientOptions);
     }
 
     protected function registerClientScript()
     {
         $view = $this->getView();
-        $this->initClientOptions();
         $editor = MarkdownEditorAsset::register($view);
-        $this->clientOptions['value'] = $this->value ? $this->value : '';
-        $this->clientOptions['name'] = $this->name;
         $this->clientOptions['path'] = $editor->baseUrl . '/lib/';
-        $jsOptions = Json::encode($this->clientOptions);
-        $id = $this->options['id'];
+
+        $jsOptions = empty ($this->clientOptions) ? '' : Json::htmlEncode($this->clientOptions);
+
+        $varName = Inflector::classify('editor' . $this->id);
+
         if ($this->clientOptions['emoji']) {
             $emoji = 'editormd.emoji = ' . Json::encode(['path' => 'http://www.webpagefx.com/tools/emoji-cheat-sheet/graphics/emojis/', 'ext' => ".png"]);
             $view->registerJs($emoji);
         }
-        $js = 'var editor = editormd("' . $id . '", ' . $jsOptions . ');';
-        $view->registerJs($js);
+        $view->registerJs("var editor{$this->id} = new editormd(\"{$varName}\", {$jsOptions});");
+        echo '<div id="' . $varName . '">' . $textarea . '</div>';
     }
     
-    public function initClientOptions()
-    {
-        $options = [];
-        $options['height'] = '600';
-        $options['watch'] = false;
-        $options['emoji'] = true;
-        $options['toolbarIcons'] = [
-            "h1",
-            "h2",
-            "bold",
-            "del",
-            "italic",
-            "quote",
-            "link",
-            "image",
-            "code-block",
-            "table",
-            "emoji",
-            "|",
-            "list-ul",
-            "list-ol",
-            "hr",
-            "|",
-            "preview",
-            "watch",
-            "fullscreen",
-        ];
-        $this->clientOptions = array_merge($options, $this->clientOptions);
-    }
+
 }
